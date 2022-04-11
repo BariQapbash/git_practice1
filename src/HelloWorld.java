44db1db9ee9ac6b78f2c36db1c7cb1ff457435af
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-04-11 10:23:59 +0200

    power folder deleted and print "Hello World" in HW class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,4 +8,9 @@ public class HelloWorld {
 
     // awesome work 3 is done and ready to be committed
     // and ready to be to remote repository
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
